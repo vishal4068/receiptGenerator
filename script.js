05e8c028ceb2fd722952c87f507ea53155708886
Vishal Garje <68316341+vishal4068@users.noreply.github.com>
2023-09-06 13:31:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function signIn(){
+    var user = document.getElementById(exampleInputEmail1).value;
+var password = document.getElementById(exampleInputPassword1).value;
+    if(user == "garjevishal4068@gmail.com" && password == "Vishal@4068"){
+            window.location.assign(invoice.html)
+    }
+}
